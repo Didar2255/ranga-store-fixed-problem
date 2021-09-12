0b2dc1596d738e36e7e390b4a3f71bced99f0731
Didar2255 <didar.a2255@gmail.com>
2021-09-12 23:16:55 +0600

    fixed image problem

--- js/app.js
+++ js/app.js
@@ -10,11 +10,10 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
+    div.innerHTML = `
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
