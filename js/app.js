509981d3773180c7d8fa68d815ef7dccbc3f6bc7
Didar2255 <didar.a2255@gmail.com>
2021-09-12 23:47:42 +0600

    fidex total product problem

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
 
-  updateTaxAndCharge();
+  updateTaxAndCharge(id);
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -45,7 +45,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
